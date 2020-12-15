7214637729f1233ede6be115a8582d5a2e3ae558
a7md0 <a7m3d699@gmail.com>
2020-12-15 20:19:35 +0300

    Remove old query and test auth context

--- NewsAgencyApp/MainForm.cs
+++ NewsAgencyApp/MainForm.cs
@@ -32,30 +32,10 @@ namespace NewsAgencyApp
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            SqlConnection con = new SqlConnection(Properties.Settings.Default.DatabaseConnectionString);
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = con;
-
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "SELECT * FROM [User];";
-
-            con.Open();
-            SqlDataReader reader = cmd.ExecuteReader();
-
-
-            while (reader.Read())
-            {
-                var value1 = reader.GetValue(0);
-                var value2 = reader.GetValue(1);
-                var value3 = reader.GetValue(2);
-
-                Console.WriteLine(value1);
-                Console.WriteLine(value2);
-                Console.WriteLine(value3);
-            }
-            
-
-            con.Close();
+            User user = AuthenticationContext.Instance().Login("admin", "admin");
+            Console.WriteLine(user.Id);
+            Console.WriteLine(user.FullName);
+            Console.WriteLine(user.Email);
         }
 
         private void loginToolStripMenuItem_Click(object sender, EventArgs e)
