1647a720ad7864d68e47bbe65df8778ddf9b316e
a7md0 <a7m3d699@gmail.com>
2020-12-15 19:24:18 +0300

    Add users models

---
+++ NewsAgencyApp/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewsAgencyApp
+{
+    class User
+    {
+        private string username;
+        private string fullname;
+        private string email;
+    }
+}
