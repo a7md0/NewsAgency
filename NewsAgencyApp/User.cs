caabf270406b584d5d97688a05ccbced53a1fe98
a7md0 <a7m3d699@gmail.com>
2020-12-15 20:17:23 +0300

    Update user model (factory method)

--- NewsAgencyApp/User.cs
+++ NewsAgencyApp/User.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NewsAgencyApp
 {
     class User
     {
-        private string username;
-        private string fullname;
-        private string email;
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+
+        public static User UserFactory(int roleId)
+        {
+            switch (roleId)
+            {
+                case 1:
+                    return new SuperUser();
+                default:
+                    throw new Exception("Unhandled user type");
+            }
+        }
     }
 }
