2a2d38fd5e76801b9cf322e8ec993bb0f024449b
a7md0 <a7m3d699@gmail.com>
2020-12-19 11:34:44 +0300

    MainForm resize

--- NewsAgencyApp/MainForm.Designer.cs
+++ NewsAgencyApp/MainForm.Designer.cs
@@ -55,7 +55,7 @@
             this.aboutToolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(596, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(602, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -114,13 +114,13 @@
             this.articlesListView.HideSelection = false;
             this.articlesListView.Location = new System.Drawing.Point(12, 36);
             this.articlesListView.Name = "articlesListView";
-            this.articlesListView.Size = new System.Drawing.Size(572, 206);
+            this.articlesListView.Size = new System.Drawing.Size(560, 206);
             this.articlesListView.TabIndex = 1;
             this.articlesListView.UseCompatibleStateImageBehavior = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(355, 255);
+            this.button1.Location = new System.Drawing.Point(576, 90);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(20, 27);
             this.button1.TabIndex = 2;
@@ -129,7 +129,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(411, 255);
+            this.button2.Location = new System.Drawing.Point(576, 143);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(20, 23);
             this.button2.TabIndex = 3;
@@ -138,7 +138,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(13, 257);
+            this.button3.Location = new System.Drawing.Point(138, 257);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(317, 23);
             this.button3.TabIndex = 4;
@@ -169,7 +169,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(596, 292);
+            this.ClientSize = new System.Drawing.Size(602, 292);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
