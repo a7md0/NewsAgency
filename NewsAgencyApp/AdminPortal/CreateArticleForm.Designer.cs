203a8a618ade894b29f0f8fd8528be557df8f1fe
a7md0 <a7m3d699@gmail.com>
2020-12-20 18:30:40 +0300

    Implement create new article

--- NewsAgencyApp/AdminPortal/CreateArticleForm.Designer.cs
+++ NewsAgencyApp/AdminPortal/CreateArticleForm.Designer.cs
@@ -28,12 +28,118 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.createButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.contentRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Items.AddRange(new object[] {
+            "--Select--"});
+            this.categoryComboBox.Location = new System.Drawing.Point(77, 206);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(238, 21);
+            this.categoryComboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Content:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Category:";
+            // 
+            // createButton
+            // 
+            this.createButton.Location = new System.Drawing.Point(240, 252);
+            this.createButton.Name = "createButton";
+            this.createButton.Size = new System.Drawing.Size(75, 23);
+            this.createButton.TabIndex = 4;
+            this.createButton.Text = "Create";
+            this.createButton.UseVisualStyleBackColor = true;
+            this.createButton.Click += new System.EventHandler(this.createButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(149, 252);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(77, 16);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(238, 20);
+            this.titleTextBox.TabIndex = 6;
+            // 
+            // contentRichTextBox
+            // 
+            this.contentRichTextBox.Location = new System.Drawing.Point(77, 56);
+            this.contentRichTextBox.Name = "contentRichTextBox";
+            this.contentRichTextBox.Size = new System.Drawing.Size(238, 134);
+            this.contentRichTextBox.TabIndex = 7;
+            this.contentRichTextBox.Text = "";
+            // 
+            // CreateArticleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CreateArticleForm";
+            this.ClientSize = new System.Drawing.Size(331, 287);
+            this.Controls.Add(this.contentRichTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.createButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.categoryComboBox);
+            this.Name = "CreateArticleForm";
+            this.Text = "Create Article";
+            this.Load += new System.EventHandler(this.CreateArticleForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button createButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.RichTextBox contentRichTextBox;
     }
 }
--- NewsAgencyApp/AdminPortal/CreateArticleForm.cs
+++ NewsAgencyApp/AdminPortal/CreateArticleForm.cs
@@ -1,20 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using NewsAgencyApp.Helper;
+using NewsAgencyApp.Models;
 
 namespace NewsAgencyApp.AdminPortal
 {
     public partial class CreateArticleForm : Form
     {
+        List<Models.Category> categoriesList = new List<Models.Category>();
+
         public CreateArticleForm()
         {
             InitializeComponent();
+        }
+
+        private void CreateArticleForm_Load(object sender, EventArgs e)
+        {
+            setupCategoriesComboBox();
+        }
+
+        private void setupCategoriesComboBox()
+        {
+            categoryComboBox.SelectedItem = null; // Default to selected null
+            categoryComboBox.SelectedText = "--Select--";  // Default to select text
+
+            categoryComboBox.DisplayMember = "Text"; // Update the combobox to display the Text value
+            categoryComboBox.ValueMember = "Value"; // Update the combobox to make the Value to be considred value
+
+            categoriesList = Models.Category.FindAll(); // Find all categories
+            this.renderCategoriesComboBox(); // Render the categories combobox method
+        }
+
+        /**
+         * Render the categories combobox items 
+        */
+        private void renderCategoriesComboBox()
+        {
+            foreach (var category in categoriesList)
+                categoryComboBox.Items.Add(new ComboboxItem() { Text = category.Name, Value = category });
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            ComboboxItem selectedItem = categoryComboBox.SelectedItem as ComboboxItem; // selected categoriy item
+
+            if (selectedItem == null)
+            {
+                MessageBox.Show("Please select a category first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (titleTextBox.Text.Length == 0 || contentRichTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Please fill the title and the content details first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Article article = new Article()
+            {
+                Title = titleTextBox.Text,
+                Content = contentRichTextBox.Text,
+                User = AuthenticationContext.Instance().State.CurrentUser, // current logged in user
+                Category = selectedItem.Value as Category // find the category object from the list by id
+            };
+            article.GenerateKeywords();
+            article.Create();
+
+            this.Close();
         }
     }
 }
