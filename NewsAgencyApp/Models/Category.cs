f71c178aa30c4283697ecf44d3127923f489e8c2
a7md0 <a7m3d699@gmail.com>
2020-12-20 12:40:45 +0300

    Add FindAll to category model

--- NewsAgencyApp/Models/Category.cs
+++ NewsAgencyApp/Models/Category.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Data;
+using System.Data.SqlClient;
+
 namespace NewsAgencyApp.Models
 {
     class Category : Model
@@ -66,6 +69,36 @@ namespace NewsAgencyApp.Models
         {
             this.name = name;
             this.numberOfArticles = 0;
+        }
+
+        public static List<Category> FindAll()
+        {
+            List<Category> categories = new List<Category>();
+
+            SqlCommand query = new SqlCommand
+            {
+                CommandText = "SELECT Id, Name, NumberOfArticle FROM [Category];",
+                Connection = DBMgr.DatabaseFactory().Connection(),
+                CommandType = CommandType.Text,
+            };
+
+            SqlDataReader sdr = query.ExecuteReader();
+
+            while (sdr.Read())
+            {
+                Category category = new Category
+                {
+                    Id = Int32.Parse(sdr["Id"].ToString()),
+                    Name = sdr["Name"].ToString(),
+                    NumberOfArticles = Int32.Parse(sdr["NumberOfArticle"].ToString()),
+                };
+
+                categories.Add(category);
+            }
+
+            sdr.Close(); // Close SqlDataReader
+
+            return categories;
         }
 
         public override bool Create()
