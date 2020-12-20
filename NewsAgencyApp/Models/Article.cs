2b23b39420ea5195626339c18f3688feeaad1fc2
a7md0 <a7m3d699@gmail.com>
2020-12-20 13:57:24 +0300

    Implement find articls by queries

--- NewsAgencyApp/Models/Article.cs
+++ NewsAgencyApp/Models/Article.cs
@@ -150,8 +150,6 @@ namespace NewsAgencyApp.Models
 
         public static List<Article> FindAll()
         {
-            List<Article> articles = new List<Article>();
-
             SqlCommand query = new SqlCommand
             {
                 CommandText = @"SELECT a.Id, a.Title, a.Content, a.NumberOfViews, a.CreatedAt, a.UpdatedAt, a.Keywords,
@@ -173,36 +171,57 @@ namespace NewsAgencyApp.Models
 
             SqlDataReader sdr = query.ExecuteReader();
 
-            while (sdr.Read())
-            {
-                User user = new User
-                {
-                    Id = Int32.Parse(sdr["UserId"].ToString()),
-                    Username = sdr["UserUserName"].ToString(),
-                    FullName = sdr["UserFullName"].ToString(),
-                    Email = null,
-                };
-                Category category = new Category
-                {
-                    Id = Int32.Parse(sdr["CategoryId"].ToString()),
-                    Name = sdr["CategoryName"].ToString(),
-                    NumberOfArticles = Int32.Parse(sdr["CategoryNumberOfArticle"].ToString()),
-                };
-                Article article = new Article
-                {
-                    id = Int32.Parse(sdr["Id"].ToString()),
-                    title = sdr["Title"].ToString(),
-                    content = sdr["Content"].ToString(),
-                    user = user,
-                    category = category,
-                    numberOfViews = Int32.Parse(sdr["NumberOfViews"].ToString()),
-                    createdAt = sdr["CreatedAt"].ToString(),
-                    updatedAt = sdr["UpdatedAt"].ToString(),
-                    keywords = sdr["Keywords"].ToString()
-                };
+            List<Article> articles = Article.parseArticles(sdr);
 
-                articles.Add(article);
+            sdr.Close(); // Close SqlDataReader
+
+            return articles;
+        }
+
+        public static List<Article> FindAll(IDictionary<string, string> filters)
+        {
+            string whereCluase = "";
+
+            if (filters.Count > 0)
+            {
+                whereCluase += "WHERE ";
+
+                foreach (var filter in filters)
+                {
+                    if (whereCluase.Length > 6)
+                        whereCluase += " AND ";
+
+                    whereCluase += filter.Value;
+                }
+                    
             }
+
+            Console.WriteLine(whereCluase);
+
+            SqlCommand query = new SqlCommand
+            {
+                CommandText = string.Format(@"SELECT a.Id, a.Title, a.Content, a.NumberOfViews, a.CreatedAt, a.UpdatedAt, a.Keywords,
+                                    c.Id as CategoryId, c.Name as CategoryName, c.NumberOfArticle as CategoryNumberOfArticle,
+                                    u.Id as UserId, u.FullName as UserFullName, u.Username as UserUserName
+
+                                    FROM [Article] AS a
+
+                                    INNER JOIN [Category] as c
+	                                    ON a.CategoryId = c.Id
+
+                                    INNER JOIN [User] as u
+	                                    ON a.UserId = u.Id
+                                    
+                                    {0}
+
+                                    ORDER BY createdAt DESC;", whereCluase),
+                Connection = DBMgr.DatabaseFactory().Connection(),
+                CommandType = CommandType.Text,
+            };
+
+            SqlDataReader sdr = query.ExecuteReader();
+
+            List<Article> articles = Article.parseArticles(sdr);
 
             sdr.Close(); // Close SqlDataReader
 
@@ -285,6 +304,45 @@ namespace NewsAgencyApp.Models
             query.ExecuteNonQuery();
 
             return;
+        }
+
+
+        private static List<Article> parseArticles(SqlDataReader sdr)
+        {
+            List<Article> articles = new List<Article>();
+
+            while (sdr.Read())
+            {
+                User user = new User
+                {
+                    Id = Int32.Parse(sdr["UserId"].ToString()),
+                    Username = sdr["UserUserName"].ToString(),
+                    FullName = sdr["UserFullName"].ToString(),
+                    Email = null,
+                };
+                Category category = new Category
+                {
+                    Id = Int32.Parse(sdr["CategoryId"].ToString()),
+                    Name = sdr["CategoryName"].ToString(),
+                    NumberOfArticles = Int32.Parse(sdr["CategoryNumberOfArticle"].ToString()),
+                };
+                Article article = new Article
+                {
+                    id = Int32.Parse(sdr["Id"].ToString()),
+                    title = sdr["Title"].ToString(),
+                    content = sdr["Content"].ToString(),
+                    user = user,
+                    category = category,
+                    numberOfViews = Int32.Parse(sdr["NumberOfViews"].ToString()),
+                    createdAt = sdr["CreatedAt"].ToString(),
+                    updatedAt = sdr["UpdatedAt"].ToString(),
+                    keywords = sdr["Keywords"].ToString()
+                };
+
+                articles.Add(article);
+            }
+
+            return articles;
         }
     }
 }
