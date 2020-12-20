9e556eab9119a0ecf2d2fa4f49ffd449385d8cf4
a7md0 <a7m3d699@gmail.com>
2020-12-20 17:25:56 +0300

    Update view article code

--- NewsAgencyApp/ViewArticle.Designer.cs
+++ NewsAgencyApp/ViewArticle.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.backButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.contentLabel = new System.Windows.Forms.Label();
+            this.detailsLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // backButton
@@ -42,46 +42,48 @@
             this.backButton.TabIndex = 0;
             this.backButton.Text = "Back";
             this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
             // 
-            // label1
+            // titleLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(106, 37);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(12, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(106, 37);
+            this.titleLabel.TabIndex = 1;
+            this.titleLabel.Text = "label1";
             // 
-            // label2
+            // contentLabel
             // 
-            this.label2.Location = new System.Drawing.Point(16, 61);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(772, 335);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.contentLabel.Location = new System.Drawing.Point(16, 61);
+            this.contentLabel.Name = "contentLabel";
+            this.contentLabel.Size = new System.Drawing.Size(772, 335);
+            this.contentLabel.TabIndex = 2;
+            this.contentLabel.Text = "label2";
             // 
-            // label3
+            // detailsLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(19, 424);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "label3";
+            this.detailsLabel.AutoSize = true;
+            this.detailsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detailsLabel.Location = new System.Drawing.Point(19, 424);
+            this.detailsLabel.Name = "detailsLabel";
+            this.detailsLabel.Size = new System.Drawing.Size(41, 13);
+            this.detailsLabel.TabIndex = 3;
+            this.detailsLabel.Text = "label3";
             // 
             // ViewArticle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.detailsLabel);
+            this.Controls.Add(this.contentLabel);
+            this.Controls.Add(this.titleLabel);
             this.Controls.Add(this.backButton);
             this.Name = "ViewArticle";
             this.Text = "ViewArticle";
+            this.Load += new System.EventHandler(this.ViewArticle_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,8 +92,8 @@
         #endregion
 
         private System.Windows.Forms.Button backButton;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label contentLabel;
+        private System.Windows.Forms.Label detailsLabel;
     }
 }
