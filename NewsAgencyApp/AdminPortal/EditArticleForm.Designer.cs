a56e57eac27f50c4c3ff74d45adde87a6e3d5ec6
a7md0 <a7m3d699@gmail.com>
2020-12-21 15:43:29 +0300

    Add edit form for article

--- NewsAgencyApp/AdminPortal/EditArticleForm.Designer.cs
+++ NewsAgencyApp/AdminPortal/EditArticleForm.Designer.cs
@@ -28,12 +28,232 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.categoryComboBox = new System.Windows.Forms.ComboBox();
+            this.contentRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.keywordsTextBox = new System.Windows.Forms.TextBox();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.authorTextBox = new System.Windows.Forms.TextBox();
+            this.idTextBox = new System.Windows.Forms.TextBox();
+            this.createdTextBox = new System.Windows.Forms.TextBox();
+            this.lastUpdatedTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(344, 401);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 0;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(263, 401);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // categoryComboBox
+            // 
+            this.categoryComboBox.FormattingEnabled = true;
+            this.categoryComboBox.Location = new System.Drawing.Point(89, 288);
+            this.categoryComboBox.Name = "categoryComboBox";
+            this.categoryComboBox.Size = new System.Drawing.Size(149, 21);
+            this.categoryComboBox.TabIndex = 2;
+            // 
+            // contentRichTextBox
+            // 
+            this.contentRichTextBox.Location = new System.Drawing.Point(89, 67);
+            this.contentRichTextBox.Name = "contentRichTextBox";
+            this.contentRichTextBox.Size = new System.Drawing.Size(330, 183);
+            this.contentRichTextBox.TabIndex = 3;
+            this.contentRichTextBox.Text = "";
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(89, 41);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(330, 20);
+            this.titleTextBox.TabIndex = 4;
+            // 
+            // keywordsTextBox
+            // 
+            this.keywordsTextBox.Location = new System.Drawing.Point(89, 262);
+            this.keywordsTextBox.Name = "keywordsTextBox";
+            this.keywordsTextBox.Size = new System.Drawing.Size(330, 20);
+            this.keywordsTextBox.TabIndex = 5;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(12, 44);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(30, 13);
+            this.titleLabel.TabIndex = 6;
+            this.titleLabel.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Content:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Keywords:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 291);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Category:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 19);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(21, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "ID:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 319);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Author:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(15, 348);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Created:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(15, 379);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(72, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Last updated:";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // authorTextBox
+            // 
+            this.authorTextBox.Location = new System.Drawing.Point(89, 316);
+            this.authorTextBox.Name = "authorTextBox";
+            this.authorTextBox.ReadOnly = true;
+            this.authorTextBox.Size = new System.Drawing.Size(149, 20);
+            this.authorTextBox.TabIndex = 17;
+            // 
+            // idTextBox
+            // 
+            this.idTextBox.Location = new System.Drawing.Point(89, 16);
+            this.idTextBox.Name = "idTextBox";
+            this.idTextBox.ReadOnly = true;
+            this.idTextBox.Size = new System.Drawing.Size(70, 20);
+            this.idTextBox.TabIndex = 18;
+            // 
+            // createdTextBox
+            // 
+            this.createdTextBox.Location = new System.Drawing.Point(89, 345);
+            this.createdTextBox.Name = "createdTextBox";
+            this.createdTextBox.ReadOnly = true;
+            this.createdTextBox.Size = new System.Drawing.Size(149, 20);
+            this.createdTextBox.TabIndex = 19;
+            // 
+            // lastUpdatedTextBox
+            // 
+            this.lastUpdatedTextBox.Location = new System.Drawing.Point(89, 379);
+            this.lastUpdatedTextBox.Name = "lastUpdatedTextBox";
+            this.lastUpdatedTextBox.ReadOnly = true;
+            this.lastUpdatedTextBox.Size = new System.Drawing.Size(149, 20);
+            this.lastUpdatedTextBox.TabIndex = 20;
+            // 
+            // EditArticleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(431, 436);
+            this.Controls.Add(this.lastUpdatedTextBox);
+            this.Controls.Add(this.createdTextBox);
+            this.Controls.Add(this.idTextBox);
+            this.Controls.Add(this.authorTextBox);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.keywordsTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.contentRichTextBox);
+            this.Controls.Add(this.categoryComboBox);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.saveButton);
+            this.Name = "EditArticleForm";
             this.Text = "EditArticleForm";
+            this.Load += new System.EventHandler(this.EditArticleForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ComboBox categoryComboBox;
+        private System.Windows.Forms.RichTextBox contentRichTextBox;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox keywordsTextBox;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox authorTextBox;
+        private System.Windows.Forms.TextBox idTextBox;
+        private System.Windows.Forms.TextBox createdTextBox;
+        private System.Windows.Forms.TextBox lastUpdatedTextBox;
     }
 }
--- NewsAgencyApp/AdminPortal/EditArticleForm.cs
+++ NewsAgencyApp/AdminPortal/EditArticleForm.cs
@@ -1,20 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using NewsAgencyApp.Helper;
+using NewsAgencyApp.Models;
 
 namespace NewsAgencyApp.AdminPortal
 {
-    public partial class EditArticleForm : Form
+    partial class EditArticleForm : Form
     {
+        private Article article;
+        private List<Category> categoriesList;
+
         public EditArticleForm()
         {
             InitializeComponent();
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EditArticleForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public Article Article
+        {
+            get
+            {
+                return article;
+            }
+
+            set
+            {
+                article = value;
+
+                this.setupCategoriesComboBox();
+                this.renderDetails();
+            }
+        }
+
+        private void renderDetails()
+        {
+            idTextBox.Text = article.Id.ToString();
+            titleTextBox.Text = article.Title;
+            contentRichTextBox.Text = article.Content;
+            keywordsTextBox.Text = article.Keywords;
+            chooseCurrentCategory(article.Category);
+            authorTextBox.Text = string.Format("{0} ({1})", article.User.FullName, article.User.Username);
+            createdTextBox.Text = article.CreatedAt;
+            lastUpdatedTextBox.Text = article.UpdatedAt;
+        }
+
+        private void setupCategoriesComboBox()
+        {
+            categoryComboBox.DisplayMember = "Text"; // Update the combobox to display the Text value
+            categoryComboBox.ValueMember = "Value"; // Update the combobox to make the Value to be considred value
+
+            categoriesList = Category.FindAll(); // Find all categories
+            this.renderCategoriesComboBox(); // Render the categories combobox method
+        }
+
+        /**
+         * Render the categories combobox items 
+        */
+        private void renderCategoriesComboBox()
+        {
+            foreach (var category in categoriesList)
+                categoryComboBox.Items.Add(new ComboboxItem { Text = category.Name, Value = category });
+        }
+
+        private void chooseCurrentCategory(Category category)
+        {
+            foreach (ComboboxItem item in categoryComboBox.Items)
+            {
+                Category c = item.Value as Category;
+                if (c.Id == category.Id)
+                {
+                    categoryComboBox.SelectedItem = item;
+                    break;
+                }
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            ComboboxItem selectedItem = categoryComboBox.SelectedItem as ComboboxItem;
+
+            article.Title = titleTextBox.Text;
+            article.Content = contentRichTextBox.Text;
+            article.Keywords = keywordsTextBox.Text;
+            article.Category = selectedItem.Value as Category;
+
+            article.Update();
+
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
