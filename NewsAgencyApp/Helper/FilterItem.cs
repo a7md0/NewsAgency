a5ebc8daa154695b68e6946c076b14c8de8565f7
a7md0 <a7m3d699@gmail.com>
2020-12-21 23:49:23 +0300

    Remove unused

--- NewsAgencyApp/Helper/FilterItem.cs
+++ NewsAgencyApp/Helper/FilterItem.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NewsAgencyApp.Helper
+﻿namespace NewsAgencyApp.Helper
 {
     class FilterItem
     {
--- NewsAgencyApp/Models/SuperUser.cs
+++ NewsAgencyApp/Models/SuperUser.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NewsAgencyApp.Models
+﻿namespace NewsAgencyApp.Models
 {
     class SuperUser : User
     {
